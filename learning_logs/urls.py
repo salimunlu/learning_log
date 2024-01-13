84ebadb51eae3aa378a3b9cc6a341ef9324d5445
salimunlu <salim.unlu@bilgeadam.com>
2024-01-13 12:06:59 +0300

    Fourth commit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),  # Bütün konuların sayfası
     path('topics/<int:topic_id>/', views.topic, name='topic'), # Konuların ayrı sayfaları
+    path('new_topic/', views.new_topic, name='new_topic'), # Yeni konu eklemek için
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import TopicForm
 from .models import Topic
 
 
@@ -21,3 +22,16 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
